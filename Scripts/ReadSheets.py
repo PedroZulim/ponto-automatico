e7417ae729f5992d1bb377ed7c19553db1cae037
PedroZulim <pedrozulim2@gmail.com>
2025-11-27 10:59:23 -0300

    csv

--- Scripts/ReadSheets.py
+++ Scripts/ReadSheets.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
 def carregar_feriados_municipais() -> None:
-        municipal_csv_url = "https://docs.google.com/spreadsheets/d/14-6PkC3XyFaSXk7WcZzsum0uBevVQfp1h063dFEOffE/edit?usp=drivesdk"
-        try:
-            print(pd.read_csv(municipal_csv_url))
-        except Exception as exc:
-            print(f"Erro ao carregar feriados municipais do Google Sheets: {exc}")
-            return
-        
+    municipal_csv_url = (
+        "https://docs.google.com/spreadsheets/d/"
+        "14-6PkC3XyFaSXk7WcZzsum0uBevVQfp1h063dFEOffE/export?format=csv"
+    )
+    try:
+        df = pd.read_csv(municipal_csv_url)
+        print(df.head())
+    except Exception as exc:
+        print(f"Erro ao carregar feriados municipais do Google Sheets: {exc}")
+        return
+
 carregar_feriados_municipais()
