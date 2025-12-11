ebb7f994cf4851c48212b80bf5d7f3bd816a2b4d
PedroZulim <pedrozulim2@gmail.com>
2025-12-11 08:44:45 -0300

    fix to access test

---
+++ Scripts/MainV2.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+
+# Adiciona o diretório Scripts ao path para importar os módulos
+sys.path.insert(0, str(Path(__file__).parent.parent / "Scripts"))
+
+from Config import Config
+from Feriados import Feriados
+
+
+def main() -> None:
+    # Carrega configurações
+    config = Config()
+    
+    feriados = Feriados(timezone=config.timezone)
+    print(feriados.get_feriados())  # Carrega os feriados para cache
+    print(feriados.is_feriado_hoje())
+    print(feriados.can_mark_today())
+
+if __name__ == "__main__":
+    main()
